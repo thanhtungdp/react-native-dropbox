883cb43afe8614071f90ac93c1f5d9afc63765f0
Tung <tungptkh@gmail.com>
2017-07-07 17:49:39 +0700

    change for starter app

--- App.js
+++ App.js
@@ -1,30 +1,24 @@
 import React from "react";
-import { StackNavigator } from "react-navigation";
 import { StyleSheet, Text, View } from "react-native";
 
-import LaunchScreen from "./screens/LaunchScreen";
-import Login from "./screens/Login";
-import Register from "./screens/Register";
-import Dashboard from "./screens/Dashboard";
-
-const AppNavigation = new StackNavigator(
-  {
-    LaunchScreen: {
-      screen: LaunchScreen
-    },
-    Login: {
-      screen: Login
-    },
-    Register: {
-      screen: Register
-    },
-	  Dashboard: {
-		  screen: Dashboard
-	  },
-  },
-  {
-    headerMode: "none"
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={style.container}>
+        <Text style={style.headerText}>Hello world</Text>
+      </View>
+    );
   }
-);
+}
 
-export default AppNavigation;
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "bold"
+  }
+});
